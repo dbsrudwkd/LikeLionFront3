b3203dba90c65c37cfdb3b4eae29e8d2f40c0fd8
yunkyeong Jang <alzkdpffk4314@gmail.com>
2022-07-08 05:39:12 +0900

    기분 추가하기

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -5,6 +5,7 @@ const DiaryEditor = () => {
     const [state, setState] = useState({
         author:"", 
         content:"",
+        emotion: 1,
     });
 
     const handleChangeState = (e) => {
@@ -13,6 +14,10 @@ const DiaryEditor = () => {
             [e.target.name]: e.target.value,
         });
     };
+
+    const handleSubmit = () => {
+        console.log(state);
+    }
 
     return(
         <div className = "DiaryEditor">
@@ -26,9 +31,26 @@ const DiaryEditor = () => {
         </div>
         <div>
             <textarea
+            name="content"
             value={state.content} 
             onChange={handleChangeState}
             />
+        </div>
+        <div>
+            <select 
+                name="emotion" 
+                value={state.emotion}
+                onChange={handleChangeState}
+            >               
+                <option value={1}>매우 별로</option>
+                <option value={2}>별로</option>
+                <option value={3}>그럭저럭</option>
+                <option value={4}>좋음</option>
+                <option value={5}>매우 좋음</option>
+            </select>
+        </div>
+        <div>
+            <button onClick={handleSubmit}>추가하기</button>
         </div>
     </div>
     );
